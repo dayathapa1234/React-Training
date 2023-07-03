1508d5164f635e1befb0984967834e4281cb2c8b
dayathapa1234 <60889983+dayathapa1234@users.noreply.github.com>
2023-07-03 16:49:44 +0100

    Add files via upload

---
+++ uplodingphoto/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './Components/Button/Button';
+
+function App() {
+  return (
+    <div className='App h-[100vh] flex justify-center'>
+      <div className="flex flex-col justify-center">
+          <Button/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
